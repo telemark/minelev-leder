459c42eee9ee4bfa43653c2575beaede43ba2338
Geir Gåsodden <geir.gasodden@pythonia.no>
2017-04-03 12:25:02 +0200

    Update dev.env. Adds missing route

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const routes = require('./routes')
 const auth = require('./routes/auth')
 const stats = require('./routes/stats')
 const reports = require('./routes/reports')
+const classes = require('./routes/classes')
 const systems = require('./routes/systems')
 
 exports.register = (server, options, next) => {
@@ -11,6 +12,7 @@ exports.register = (server, options, next) => {
   server.route(auth)
   server.route(stats)
   server.route(reports)
+  server.route(classes)
   server.route(systems)
   next()
 }
