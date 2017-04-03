17c187394c8363e9161778dd949a80d9241803bb
Geir Gåsodden <geir.gasodden@pythonia.no>
2017-04-03 12:41:55 +0200

    Support for classes

--- handlers/classes.js
+++ handlers/classes.js
@@ -1,5 +1,6 @@
 'use strict'
 
+const logger = require('../lib/logger')
 const resolveClasses = require('../lib/resolve-classes')
 const createViewOptions = require('../lib/create-view-options')
 
@@ -9,9 +10,11 @@ module.exports.getClasses = async (request, reply) => {
   const isAdmin = yar.get('isAdmin') || false
   const mySchools = yar.get('mySchools') || []
   let myClasses = yar.get('myClasses') || []
+  const schoolId = request.params.schoolID
 
   if (myClasses.length === 0) {
-    const selectedClasses = await resolveClasses({id: userId, schools: request.params.schoolId})
+    logger('classes', 'looking up classes', schoolId)
+    const selectedClasses = await resolveClasses({id: userId, schoolId: schoolId})
     const mySchoolIds = mySchools.map(school => school.id)
     myClasses = selectedClasses.filter(c => mySchoolIds.includes(c.schoolId))
     yar.set('myClasses', myClasses)
