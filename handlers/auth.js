5e2ab3ce851e64b04e81fa1a7a5c5704bb611ed3
Geir Gåsodden <geir.gasodden@pythonia.no>
2017-04-20 11:00:08 +0200

    Adds verify and encryption

--- handlers/auth.js
+++ handlers/auth.js
@@ -1,30 +1,38 @@
 'use strict'
 
 const lookupUser = require('../lib/lookup-user')
+const verifyJwt = require('../lib/verify-jwt')
 const logger = require('../lib/logger')
 
 module.exports.doSignIn = async (request, reply) => {
   const token = request.query.jwt
   const nextPath = request.query.nextPath
   const yar = request.yar
-  try {
-    const user = await lookupUser(token)
-    logger('info', ['auth', 'doSignIn', 'user verified', 'userId', user.userId])
+  const check = await verifyJwt(token)
+  if (check.isValid === true) {
+    logger('info', ['auth', 'doSignIn', 'token is valid'])
+    try {
+      const user = await lookupUser(token)
+      logger('info', ['auth', 'doSignIn', 'user verified', 'userId', user.userId])
 
-    yar.set('isAdmin', user.isAdmin)
-    yar.set('mySchools', user.mySchools)
-    yar.set('myClasses', [])
+      yar.set('isAdmin', user.isAdmin)
+      yar.set('mySchools', user.mySchools)
+      yar.set('myClasses', [])
 
-    request.cookieAuth.set({data: user, token: token})
+      request.cookieAuth.set({data: user, token: token})
 
-    if (nextPath && nextPath.length > 0) {
-      reply.redirect(nextPath)
-    } else {
-      reply.redirect('/')
+      if (nextPath && nextPath.length > 0) {
+        reply.redirect(nextPath)
+      } else {
+        reply.redirect('/')
+      }
+    } catch (error) {
+      logger('error', ['auth', 'doSignIn', 'error', error])
+      reply(error)
     }
-  } catch (error) {
-    logger('error', ['auth', 'doSignIn', 'error', error])
-    reply(error)
+  } else {
+    logger('error', ['auth', 'doSignIn', 'invalid token'])
+    reply(new Error('Invalid token'))
   }
 }
 
